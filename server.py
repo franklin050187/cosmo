5b1313a539185fae042f64140288f01adb039b3d
franklin050187 <cat050187@gmail.com>
2023-05-22 21:57:59 +0200

    added upload restriction to server join

--- server.py
+++ server.py
@@ -4,24 +4,22 @@ from fastapi import (
     Request,
     File,
     UploadFile,
-    HTTPException,
 )
 from fastapi.responses import (
     FileResponse,
     RedirectResponse,
     HTMLResponse,
-    JSONResponse,
 )
 from fastapi.templating import Jinja2Templates
-from starlette.exceptions import HTTPException
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
-from starlette.responses import RedirectResponse
-from starlette_discord.client import DiscordOAuthClient, User
+from starlette.responses import RedirectResponse, JSONResponse
+from starlette_discord.client import DiscordOAuthClient
 import sqlite3
 import base64
 import os
 from dotenv import load_dotenv
+
 import json
 
 load_dotenv()
@@ -29,9 +27,10 @@ load_dotenv()
 # Discord OAuth2 settings
 client_id = os.getenv('discord_id')
 client_secret = os.getenv('discord_secret')
-redirect_uri = "http://localhost:8000/callback"
+redirect_uri = os.getenv('discord_redirect')
+print(redirect_uri)
 
-client = DiscordOAuthClient(client_id, client_secret, redirect_uri)
+client = DiscordOAuthClient(client_id, client_secret, redirect_uri, ("identify", "guilds"))
 
 # Create a table in the database if it doesn't exist
 def create_table():
@@ -55,21 +54,27 @@ async def start_login():
 
 @app.get('/callback')
 async def finish_login(code: str):
-    user = await client.login(code)
-    print(user)
-    path = 'templates/upload.html'
+    async with client.session(code) as session:
+        user = await session.identify()
+        guilds = await session.guilds()
+        # get guilds and parse them
+        # Access the parsed data
+        desired_id = int(os.getenv('guild_id')) #Excelsior server
+        for guild in guilds:
+            if guild.id == desired_id:
+                path = 'templates/upload.html'
+                return FileResponse(path)
+    # redirect to join the server before uploading
+    path = 'templates/auth.html'
     return FileResponse(path)
 
+# Endpoint for displaying the file upload page
 @app.get("/upload", response_class=FileResponse)
 async def upload_page(request: Request):
-    print("upload")
     user = request.session.get("discord_user")
-    print(user)
     if not user:
         print("not a user upload")
-        #raise HTTPException(status_code=401, detail="Unauthorized")
         return RedirectResponse("/login")
-    print("a user upload")
     path = 'templates/upload.html'
     return FileResponse(path)
 
@@ -78,17 +83,14 @@ async def upload_page(request: Request):
 async def upload(file: UploadFile = File(...)):
     # Read the image file
     contents = await file.read()
-
     # Encode the contents as base64
     encoded_data = base64.b64encode(contents).decode("utf-8")
-
     # Store the image data in the database
     conn = sqlite3.connect('example.db')
     cursor = conn.cursor()
     cursor.execute('INSERT INTO images (name, data) VALUES (?, ?)', (file.filename, encoded_data))
     conn.commit()
     conn.close()
-
     # Redirect to the index page
     return RedirectResponse(url="/", status_code=303)
 
@@ -100,11 +102,7 @@ async def home(request: Request):
     cursor.execute('SELECT name, data FROM images')
     images = cursor.fetchall()
     conn.close()
-
     return templates.TemplateResponse("index.html", {"request": request, "images": images})
-
-# Endpoint for displaying the file upload page
-
 
 # Catch-all endpoint for serving static files or the index page
 @app.get("/{catchall:path}", response_class=FileResponse)
@@ -112,15 +110,11 @@ def serve_files(request: Request):
     # Check if the requested file exists
     path = request.path_params["catchall"]
     file = 'templates/' + path
-
     if os.path.exists(file):
         return FileResponse(file)
-
     # Otherwise, return the index file
     index = 'templates/index.html'
     return FileResponse(index)
-
-
 
 if __name__ == '__main__':
     uvicorn.run(app)
