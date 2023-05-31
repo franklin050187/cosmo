261da470543bcf9b4e8fa85c36198041b30eabd6
franklin050187 <cat050187@gmail.com>
2023-05-31 18:23:23 +0200

    assume tmp is mounted by default

--- server.py
+++ server.py
@@ -42,14 +42,8 @@ client = DiscordOAuthClient(
 # app configuration
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-#app.mount("/tmp", StaticFiles(directory="tmp"), name="tmp")
+#app.mount("/tmp", StaticFiles(directory="tmp"), name="tmp") # mounting tmp crashed the serverless function
 templates = Jinja2Templates(directory="templates")
-
-# tmp test
-tmp_directory = "tmp"
-if not os.path.exists(tmp_directory):
-    os.makedirs(tmp_directory)
-app.mount("/tmp", StaticFiles(directory=tmp_directory), name="tmp")
 
 # postgresql configuration (db and table must be setup before use)
 def connect_to_server():
@@ -77,36 +71,36 @@ def get_image(id: int, request: Request):
     conn.commit()
     conn.close()
     
-    # ### dirty edit ###
-    # script_dir = os.path.dirname(os.path.abspath(__file__))
+    ### dirty edit ### lets assume the TMP is mounted by default and try
+    script_dir = os.path.dirname(os.path.abspath(__file__))
  
-    # # Create folder if it doesn't exist
-    # folder_path = os.path.join(script_dir, f"tmp/{id}")
-    # os.makedirs(folder_path, exist_ok=True)
+    # Create folder if it doesn't exist
+    folder_path = os.path.join(script_dir, f"tmp/{id}")
+    os.makedirs(folder_path, exist_ok=True)
 
-    # # Decode base64 image data
-    # image_bytes = base64.b64decode(image_data[2])
+    # Decode base64 image data
+    image_bytes = base64.b64decode(image_data[2])
 
-    # # Generate filename using image_data[1]
-    # filename = image_data[1]
+    # Generate filename using image_data[1]
+    filename = image_data[1]
 
-    # # Save the image file in the created folder
-    # file_path = os.path.join(folder_path, filename)
+    # Save the image file in the created folder
+    file_path = os.path.join(folder_path, filename)
     
-    # # Replace backslash with forward slash in the file path
-    # if not os.path.exists(file_path):
-    #     with open(file_path, "wb") as f:
-    #         f.write(image_bytes)
-    # else:
-    #     print("File already exists. Skipping creation.")
+    # Replace backslash with forward slash in the file path
+    if not os.path.exists(file_path):
+        with open(file_path, "wb") as f:
+            f.write(image_bytes)
+    else:
+        print("File already exists. Skipping creation.")
         
-    # print(file_path)
+    print(file_path)
     
     # Pass the image data and file path to the template
-    # return templates.TemplateResponse("ship.html", {"request": request, "image": image_data, "user": user, "file_path": file_path})
-    # ### dirty edit ###
+    return templates.TemplateResponse("ship.html", {"request": request, "image": image_data, "user": user, "file_path": file_path})
+    ### dirty edit ###
     
-    return templates.TemplateResponse("ship.html", {"request": request, "image": image_data, "user": user})
+    #return templates.TemplateResponse("ship.html", {"request": request, "image": image_data, "user": user})
 
 # delete user ship
 @app.get("/delete/{id}", response_class=HTMLResponse)
