77a80e1a67df5a8f2e704d34bbab657b64140e4c
franklin050187 <cat050187@gmail.com>
2023-06-01 18:56:00 +0200

    revert code cleanup

--- server.py
+++ server.py
@@ -30,7 +30,6 @@ from starlette_discord.client import DiscordOAuthClient
 from png_upload import upload_image_to_imgbb
 from tagextractor import PNGTagExtractor
 import requests
-from db import upload_image, get_image_data, get_image_url
 
 load_dotenv()
 
@@ -68,10 +67,17 @@ async def start_login():
 @app.get("/ship/{id}", response_class=HTMLResponse)
 def get_image(id: int, request: Request):
     user = request.session.get("discord_user")
+    # Retrieve image information from the database based on the provided ID
+    conn = connect_to_server()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM images WHERE id=%s", (id,))
+    image_data = cursor.fetchone()
+    conn.commit()
+    conn.close()
     
-    image_data = get_image_data(id)
-    url_png = get_image_url(image_data)
-    
+    # upload to imagebb and get url
+    url_png = upload_image_to_imgbb(image_data[2])
+        
     return templates.TemplateResponse("ship.html", {"request": request, "image": image_data, "user": user, "url_png": url_png})
 
 # delete user ship
@@ -207,8 +213,64 @@ async def finish_login(code: str, request: Request):
 @app.post("/upload")
 async def upload(request: Request):
     user = request.session.get("discord_user")
+    conn = connect_to_server()
+    cursor = conn.cursor()
+
+    # Prepare the data
     form_data = await request.form()
-    upload_image(form_data, user)
+    
+    url_png = form_data.get('url_png')
+
+    response = requests.get(url_png)
+    image_data = response.content
+
+    # Encode image data as base64
+    base64_image = base64.b64encode(image_data).decode('utf-8')
+    
+    # print(base64_image)
+
+    image_data = {
+        'name': form_data.get('filename', ''),
+        'data': base64_image,
+        'submitted_by': user,
+        'description': form_data.get('description', ''),
+        'ship_name': form_data.get('ship_name', ''),
+        'author': form_data.get('author', ''),
+        'price': int(form_data.get('price', 0))
+    }
+
+    # Define the column names
+    columns = ['name', 'data', 'submitted_by', 'description', 'ship_name', 'author', 'price']
+
+    # Prepare the values
+    values = [image_data[column] for column in columns]
+
+    # Prepare the boolean columns
+    boolean_columns = [
+        'cannon', 'deck_cannon', 'emp_missiles', 'flak_battery', 'he_missiles', 'large_cannon', 'mines', 'nukes',
+        'railgun', 'ammo_factory', 'emp_factory', 'he_factory', 'mine_factory', 'nuke_factory', 'disruptors',
+        'heavy_Laser', 'ion_Beam', 'ion_Prism', 'laser', 'mining_Laser', 'point_Defense', 'boost_thruster',
+        'airlock', 'campaign_factories', 'explosive_charges', 'fire_extinguisher', 'no_fire_extinguishers',
+        'large_reactor', 'large_shield', 'medium_reactor', 'sensor', 'small_hyperdrive', 'small_reactor',
+        'small_shield', 'tractor_beams', 'hyperdrive_relay', 'bidirectional_thrust', 'mono_thrust', 'multi_thrust',
+        'omni_thrust', 'armor_defenses', 'mixed_defenses', 'shield_defenses', 'Corvette', 'diagonal', 'flanker',
+        'mixed_weapons', 'painted', 'unpainted', 'splitter', 'utility_weapons', 'transformer', 'campaign_ship', 'factories'
+    ]
+
+    # Prepare the boolean values
+    boolean_values = [1 if column in form_data else 0 for column in boolean_columns]
+
+    # Construct the SQL query
+    query = f"INSERT INTO images ({', '.join(columns + boolean_columns)}) VALUES ({', '.join(['%s'] * (len(columns) + len(boolean_columns)))})"
+
+    # Execute the query
+    cursor.execute(query, tuple(values + boolean_values))
+
+    # Commit and close the connection
+    conn.commit()
+    conn.close()
+
+    # Redirect to the index page
     return RedirectResponse(url="/", status_code=303)
 
 # Endpoint for displaying the file initupload page
