c9ae4d3061aedabe79248aa0c29a1104dfa00588
franklin050187 <cat050187@gmail.com>
2023-06-01 20:34:40 +0200

    cleanup

--- testb64.py
+++
@@ -1,5 +0,0 @@
-import requests
-import base64
-url = 'https://i.ibb.co/t3HZjBz/09480b726230.png'
-base64img = base64.b64encode(requests.get(url).content)
-print(base64img)
--- testpng.py
+++
@@ -1,5 +0,0 @@
-from tagextractor import PNGTagExtractor
-
-extractor = PNGTagExtractor()
-tags = extractor.extract_tags("https://i.ibb.co/5L1W44Z/47bdfd1a8046.png")
-print(tags)
