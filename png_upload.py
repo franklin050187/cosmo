fb36fd571718ede3b9c231d4a110e09062135fdc
franklin050187 <cat050187@gmail.com>
2023-06-07 16:44:43 +0200

    png fallback for upload

--- png_upload.py
+++ png_upload.py
@@ -1,6 +1,6 @@
 import requests
 import os
-import base64
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,21 +9,52 @@ def upload_image_to_imgbb(image_base64):
     api_key = os.getenv('imagebb_api')
     url = "https://api.imgbb.com/1/upload"
 
-    # Decode the base64 image data
-    # image_data = base64.b64decode(image_base64)
-
     # Prepare the upload request
     payload = {
         "key": api_key,
         "image": image_base64
     }
 
-    # Send the upload request
-    response = requests.post(url, payload)
-    print(response)
-    # Parse the response
-    data = response.json()
-    print(data)
-    image_url = data["data"]["url"]
+    try:
+        # Send the upload request
+        response = requests.post(url, payload)
+        response.raise_for_status()  # Raise an exception for non-2xx status codes
 
-    return image_url
+        # Parse the response
+        data = response.json()
+        image_url = data["data"]["url"]
+
+        return image_url
+    except requests.exceptions.RequestException as e:
+        print("fallback : ", e)
+        return upload_image_to_cloudinary(image_base64)
+
+def upload_image_to_cloudinary(image_base64):
+    # Set your Cloudinary credentials
+    cloudinary_url = "https://api.cloudinary.com/v1_1/{cloud_name}/image/upload"
+    api_key = os.getenv('cloud_api_key')
+    api_secret = os.getenv('cloud_api_secret')
+    cloud_name = os.getenv('cloud_name')
+    upload_preset = os.getenv('upload_preset')
+
+    # Create the payload
+    payload = {
+        "file": "data:image/png;base64,"+image_base64,
+        "upload_preset": "xttg4crr",
+        "api_key": api_key,
+        "api_secret": api_secret,
+    }
+
+    # Make the POST request
+    response = requests.post(cloudinary_url.format(cloud_name=cloud_name), data=payload)
+
+    # Process the response
+    if response.status_code == 200:
+        json_response = response.json()
+        # Extract the URL of the uploaded image
+        image_url = json_response["secure_url"]
+        print("Image uploaded successfully. URL:", image_url)
+        return image_url
+    else:
+        print("Error uploading image:", response.text)
+        return "ko"
